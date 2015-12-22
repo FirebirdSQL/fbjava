3fd579f8c25bd8bd5d6fe518fcdaaca2890ca8a4
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2015-12-22 12:19:37 -0200

    License.

--- src/java/src/main/java/org/firebirdsql/fbjava/ExternalFunction.java
+++ src/java/src/main/java/org/firebirdsql/fbjava/ExternalFunction.java
@@ -1,3 +1,21 @@
+/*
+ * FB/Java plugin
+ *
+ * Distributable under LGPL license.
+ * You may obtain a copy of the License at http://www.gnu.org/copyleft/lgpl.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * LGPL License for more details.
+ *
+ * This file was created by members of the Firebird development team.
+ * All individual contributions remain the Copyright (C) of those
+ * individuals.  Contributors to this file are either listed here or
+ * can be obtained from a git log command.
+ *
+ * All rights reserved.
+ */
 package org.firebirdsql.fbjava;
 
 import java.lang.reflect.InvocationTargetException;
