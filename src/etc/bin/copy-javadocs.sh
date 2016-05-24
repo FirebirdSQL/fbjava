f95fe1c6308e4e8aade71ecf192f0cca766355fc
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2016-05-24 14:22:16 -0300

    Script to copy javadocs to /apidocs

---
+++ src/etc/bin/copy-javadocs.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+THIS_DIR=`readlink -f $0`
+THIS_DIR=`dirname $THIS_DIR`
+
+if [ "$OS" = "Windows_NT" ]; then
+	THIS_DIR=`echo "$THIS_DIR" | tr \\\\ /`
+fi
+
+BASE_DIR=$THIS_DIR/../../..
+
+rm -rf $BASE_DIR/apidocs
+cp -rf $BASE_DIR/src/fbjava/target/apidocs $BASE_DIR
