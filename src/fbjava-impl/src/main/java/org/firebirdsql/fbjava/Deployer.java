74b077c1e0480a5c802ca8dfc04fbebe91e9f2e5
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2020-07-06 22:13:46 -0300

    Fix warnings.

--- src/fbjava-impl/src/main/java/org/firebirdsql/fbjava/Deployer.java
+++ src/fbjava-impl/src/main/java/org/firebirdsql/fbjava/Deployer.java
@@ -21,7 +21,6 @@ package org.firebirdsql.fbjava;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.URL;
--- src/fbjava-impl/src/main/java/org/firebirdsql/fbjava/impl/Main.java
+++ src/fbjava-impl/src/main/java/org/firebirdsql/fbjava/impl/Main.java
@@ -34,7 +34,7 @@ final class Main
 	public static void initialize(final String nativeLibrary) throws ClassNotFoundException
 	{
 		//// FIXME: Receive the client library name from the plugin.
-		library = (FbClientLibrary) Native.loadLibrary("fbclient", FbClientLibrary.class);
+		library = (FbClientLibrary) Native.load("fbclient", FbClientLibrary.class);
 
 		//// FIXME: Receive the master interface from the plugin.
 		master = library.fb_get_master_interface();
